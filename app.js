2a0c220f0fe434779c1ca75712fe0a55d97525af
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-07-08 22:48:42 +0300

    PORT instead of port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
